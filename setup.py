31c899b44249a72507777e6c409bae3338b5b36c
Jay Mavani <jaymavanime39@gmail.com>
2026-07-17 14:37:58 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is an end to end project in machine learning with experimentation using mlflow, model registery and model serving using flask.',
+    author='Jay Mavani',
+    license='MIT',
+)
